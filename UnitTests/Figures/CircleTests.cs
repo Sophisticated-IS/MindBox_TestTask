b17e79988abf02780047749aa09b6fc76db36bda
Igor SI <sowa.igor2012@ya.ru>
2022-08-21 22:48:15 +0500

    добавил базовый случай теста для проверки площади круга

---
+++ UnitTests/Figures/CircleTests.cs
@@ -0,0 +1,29 @@
+﻿using FiguresLibrary.Models.Figures;
+using Xunit;
+
+namespace UnitTests.Figures;
+
+public sealed class CircleTests
+{
+    [Fact]
+    public void TestSquareCalculation()
+    {
+        //Arrange
+        const double radius = 10d;
+        const double expected = 314.159265;
+
+        //Act
+        var circle = new Circle
+        {
+            Arguments = new[]
+            {
+                radius
+            }
+        };
+        circle.MapArguments();
+        var actual = circle.CalculateSquare();
+
+        //Assert
+        Assert.Equal(expected,actual);
+    }
+}
