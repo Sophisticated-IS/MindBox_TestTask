188d6b8961dba600fec10626f958239cf54758a5
Igor SI <sowa.igor2012@ya.ru>
2022-08-21 23:03:34 +0500

    добавил базовый тест для расчета площади треугольника

---
+++ UnitTests/Figures/TriangleTests.cs
@@ -0,0 +1,33 @@
+﻿using FiguresLibrary.Models.Figures;
+using Xunit;
+
+namespace UnitTests.Figures;
+
+public sealed class TriangleTests
+{
+    [Fact]
+    public void TestCalculateSquare()
+    {
+        //Arrange
+        const double sideA = 3;
+        const double sideB = 4;
+        const double sideC = 5;
+        const double expected = 6;
+
+        //Act
+        var circle = new Triangle()
+        {
+            Arguments = new[]
+            {
+                sideA,
+                sideB,
+                sideC
+            }
+        };
+        circle.MapArguments();
+        var actual = circle.CalculateSquare();
+
+        //Assert
+        Assert.Equal(expected,actual,Math.Pow(10,-6));
+    }
+}
