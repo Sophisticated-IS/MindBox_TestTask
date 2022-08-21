5df1010e4cb7e7ffd8e9c19ab08c568fb3530012
Igor SI <sowa.igor2012@ya.ru>
2022-08-21 18:07:19 +0500

    создал фабричный метод для фигур

---
+++ FiguresLibrary/FigureFactory.cs
@@ -0,0 +1,17 @@
+﻿using FiguresLibrary.Models;
+using FiguresLibrary.Models.Base;
+
+namespace FiguresLibrary;
+
+public sealed class FigureFactory
+{
+    public Shape CreateFigure(string id)
+    {
+        return id switch
+        {
+            "Circle" => new Circle(),
+            "Triangle" => new Triangle(),
+            _ => throw new ArgumentException("Figure was not found!", nameof(id))
+        };
+    } 
+}
